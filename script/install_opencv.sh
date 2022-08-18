a217f7d011e381c29dcc83a48144708503ba88d2
RiccardoPeruffo96 <riccardoperuffo96@gmail.com>
2022-08-18 18:05:31 +0200

    add 3rd check

--- script/install_opencv.sh
+++ script/install_opencv.sh
@@ -2,7 +2,7 @@
 
 echo "-- start cloning opencv..."
 wget -O $1/opencv.zip https://github.com/opencv/opencv/archive/master.zip
-unzip $1/opencv.zip
+unzip $1/opencv.zip -d $1
 mv $1/opencv-master $1/opencv
 
 echo "-- moving to opencv/build"
@@ -10,7 +10,7 @@ mkdir $1/opencv/build
 cd $1/opencv/build
 
 echo "-- calling cmake..."
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DOPENCV_DOWNLOAD_PATH=/tmp/opencv-cache..
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release -DOPENCV_DOWNLOAD_PATH=/tmp/opencv-cache ..
 
 echo "-- compiling jsoncpp"
 make
